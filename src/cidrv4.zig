f70ac4cffc7c444cb47ad6611caaca535e0713ce
sloweax <141693289+sloweax@users.noreply.github.com>
2025-05-13 17:11:57 -0300

    add support for cidrs with (ip & ~mask) != 0

--- src/cidrv4.zig
+++ src/cidrv4.zig
@@ -14,7 +14,7 @@ const Iterator = struct {
 
     pub fn next(self: *@This()) ?u32 {
         if (self.eof) return null;
-        var tmp = self.cidr.ipv4;
+        var tmp = self.cidr.ipv4 & self.cidr.mask;
         tmp |= self.cur;
         if (self.cur >= self.end) {
             self.eof = true;
@@ -35,7 +35,10 @@ pub fn max(self: Self) u32 {
 }
 
 pub fn contains(self: Self, ipv4: u32) bool {
-    return (self.ipv4 == (ipv4 & self.mask));
+    const a = self.ipv4 & self.mask;
+    const b = ipv4 & self.mask;
+    if (a == b) return ipv4 >= self.ipv4;
+    return false;
 }
 
 pub fn parse(cidr: []const u8) !Self {
@@ -49,12 +52,15 @@ pub fn parse(cidr: []const u8) !Self {
     if (b > 32) return error.InvalidCIDRv4;
     var m: u32 = std.math.maxInt(u32);
     m = std.math.shl(u32, m, 32 - b);
-    if (a & (~m) != 0) return error.InvalidCIDRv4;
     return .{ .ipv4 = a, .mask = m, .bits = @intCast(b) };
 }
 
 pub fn iterator(self: Self) Iterator {
-    return .{ .cidr = self, .end = @intCast(std.math.pow(u33, 2, 32 - self.bits) - 1) };
+    return .{
+        .cidr = self,
+        .end = @intCast(std.math.pow(u33, 2, 32 - self.bits) - 1),
+        .cur = self.ipv4 & (~self.mask),
+    };
 }
 
 pub fn parseIPv4(ip: []const u8) !u32 {
--- src/cidrv4_test.zig
+++ src/cidrv4_test.zig
@@ -2,12 +2,16 @@ const std = @import("std");
 const CIDRv4 = @import("cidrv4.zig");
 
 test "test parse" {
-    const cidr = try CIDRv4.parse("192.168.10.0/24");
+    var cidr = try CIDRv4.parse("192.168.10.0/24");
     try std.testing.expect(cidr.bits == 24);
     try std.testing.expect(cidr.mask == 0xffffff00);
     try std.testing.expect(cidr.ipv4 == 0xc0a80a00);
+    cidr = try CIDRv4.parse("192.168.10.1/24");
+    try std.testing.expect(cidr.bits == 24);
+    try std.testing.expect(cidr.mask == 0xffffff00);
+    try std.testing.expect(cidr.ipv4 == 0xc0a80a01);
 
-    try std.testing.expectError(error.InvalidCIDRv4, CIDRv4.parse("192.168.10.1/24"));
+    try std.testing.expectError(error.InvalidCIDRv4, CIDRv4.parse("192.168.10.0/33"));
     try std.testing.expectError(error.InvalidIPv4, CIDRv4.parse("192.168.10.p/24"));
 }
 
@@ -45,4 +49,21 @@ test "test min max" {
     cidr = try CIDRv4.parse("0.0.0.0/0");
     try std.testing.expect(cidr.min() == try CIDRv4.parseIPv4("0.0.0.0"));
     try std.testing.expect(cidr.max() == try CIDRv4.parseIPv4("255.255.255.255"));
+    cidr = try CIDRv4.parse("192.168.10.1/24");
+    try std.testing.expect(cidr.min() == try CIDRv4.parseIPv4("192.168.10.1"));
+    try std.testing.expect(cidr.max() == try CIDRv4.parseIPv4("192.168.10.255"));
+}
+
+test "test contains" {
+    var cidr = try CIDRv4.parse("192.168.10.0/24");
+    try std.testing.expect(cidr.contains(try CIDRv4.parseIPv4("192.168.10.0")));
+    try std.testing.expect(cidr.contains(try CIDRv4.parseIPv4("192.168.10.255")));
+    try std.testing.expect(!cidr.contains(try CIDRv4.parseIPv4("192.168.11.0")));
+    try std.testing.expect(!cidr.contains(try CIDRv4.parseIPv4("192.168.9.0")));
+    cidr = try CIDRv4.parse("192.168.10.1/24");
+    try std.testing.expect(!cidr.contains(try CIDRv4.parseIPv4("192.168.10.0")));
+    try std.testing.expect(cidr.contains(try CIDRv4.parseIPv4("192.168.10.1")));
+    try std.testing.expect(cidr.contains(try CIDRv4.parseIPv4("192.168.10.255")));
+    try std.testing.expect(!cidr.contains(try CIDRv4.parseIPv4("192.168.11.0")));
+    try std.testing.expect(!cidr.contains(try CIDRv4.parseIPv4("192.168.9.0")));
 }
